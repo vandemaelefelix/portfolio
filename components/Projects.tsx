9960a4b0f0f65cf2dd07f2853ae16293118c1b8a
Felix Vandemaele <vandemaelefelix@gmail.com>
2022-04-08 18:23:33 +0200

    Fixed resizing issues on mobile in projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -28,7 +28,7 @@ export default function Projects({ isMobile }: any) {
 
             horizontalScroll = gsap.to('#horizontalSection', {
                 xPercent: () => -100 * (projects.length - 1),
-                ease: 'ease',
+                ease: 'linear',
                 scrollTrigger: {
                     id: 'horizontalScroll',
                     scroller: '#mainSection',
@@ -79,10 +79,6 @@ export default function Projects({ isMobile }: any) {
         }
 
         return () => {
-            // if (horizontalScroll) {
-            //     horizontalScroll.kill(true);
-            //     ScrollTrigger.getById('horizontalScroll').kill(true);
-            // }
             killHorizontalScroll();
         };
     }, [isMobile]);
@@ -92,84 +88,86 @@ export default function Projects({ isMobile }: any) {
 
         projects.forEach((project) => {
             projectsContent.push(
-                <div key={project.id} id={`section${project.id}`} className={`${styles.projectContainer}`}>
-                    <div className={`${styles.project}`}>
-                        <h1 className={`${styles.title}`}>{project.name}</h1>
-                        <p className={`${styles.description}`}>{project.description}</p>
-                        <div className={`${styles.imageContainer}`}>
-                            <svg
-                                ref={(element) => {
-                                    if (parrallaxRefs.current) {
-                                        parrallaxRefs.current[1] = {
-                                            element: element,
-                                            options: { speed: 0.012, direction: 'invert' },
-                                        };
-                                    }
-                                }}
-                                className={`
+                <>
+                    <div key={project.id} id={`section${project.id}`} className={`${styles.projectContainer}`}>
+                        <div className={`${styles.project}`}>
+                            <h1 className={`${styles.title}`}>{project.name}</h1>
+                            <p className={`${styles.description}`}>{project.description}</p>
+                            <div className={`${styles.imageContainer}`}>
+                                <svg
+                                    ref={(element) => {
+                                        if (parrallaxRefs.current) {
+                                            parrallaxRefs.current[parrallaxRefs.current.length + 1] = {
+                                                element: element,
+                                                options: { speed: 0.012, direction: 'invert' },
+                                            };
+                                        }
+                                    }}
+                                    className={`
                                     ${styles.imageIcon}
                                     ${styles.top}
                                 `}
-                                viewBox="0 0 134.46 134.46"
-                            >
-                                <path
-                                    d="M0,0C23.477,23.477,70.047,24.356,94.4,0c-24.364,24.364-27.628,68.124,0,95.752-28.039-28.039-68.966-25.438-94.4,0C25.258,70.494,23.457,23.457,0,0Z"
-                                    transform="translate(67.707) rotate(45)"
-                                />
-                            </svg>
-                            <div className={`${styles.imageWrapper}`}>
-                                <Image
-                                    className={styles.image}
-                                    alt={project.name}
-                                    src={`/images/${project.image}`}
-                                    layout={'fill'}
-                                    objectFit={'cover'}
-                                    loading={'lazy'}
-                                ></Image>
-                            </div>
-                            <div className={styles.imageOverlay}></div>
-                            <svg
-                                ref={(element) => {
-                                    if (parrallaxRefs.current) {
-                                        parrallaxRefs.current[2] = {
-                                            element: element,
-                                            options: { speed: 0.012, direction: 'invert' },
-                                        };
-                                    }
-                                }}
-                                className={`
+                                    viewBox="0 0 134.46 134.46"
+                                >
+                                    <path
+                                        d="M0,0C23.477,23.477,70.047,24.356,94.4,0c-24.364,24.364-27.628,68.124,0,95.752-28.039-28.039-68.966-25.438-94.4,0C25.258,70.494,23.457,23.457,0,0Z"
+                                        transform="translate(67.707) rotate(45)"
+                                    />
+                                </svg>
+                                <div className={`${styles.imageWrapper}`}>
+                                    <Image
+                                        className={styles.image}
+                                        alt={project.name}
+                                        src={`/images/${project.image}`}
+                                        layout={'fill'}
+                                        objectFit={'cover'}
+                                        loading={'lazy'}
+                                    ></Image>
+                                </div>
+                                <div className={styles.imageOverlay}></div>
+                                <svg
+                                    ref={(element) => {
+                                        if (parrallaxRefs.current) {
+                                            parrallaxRefs.current[parrallaxRefs.current.length + 1] = {
+                                                element: element,
+                                                options: { speed: 0.012, direction: 'invert' },
+                                            };
+                                        }
+                                    }}
+                                    className={`
                                     ${styles.imageIcon}
                                     ${styles.bottom}
                                 `}
-                                viewBox="0 0 249.734 249.734"
-                            >
-                                <path
-                                    d="M0,0C43.6,43.6,130.1,45.237,175.336,0c-45.251,45.251-51.313,126.528,0,177.841C123.259,125.763,47.245,130.6,0,177.841,46.912,130.929,43.567,43.567,0,0Z"
-                                    transform="translate(125.753) rotate(45)"
-                                />
-                            </svg>
-                        </div>
-                        <div className={`${styles.link}`}>
-                            <a
-                                data-mouse={'hide'}
-                                // className={`${styles.link}`}
-                                target="_blank"
-                                rel="noreferrer"
-                                href={project.link}
-                            >
-                                see more
-                                <span data-mouse={'hide'} className={`${styles.arrow}`}>
-                                    <svg viewBox="0 0 330.001 180">
-                                        <path
-                                            d="M100.606,100.606,150,51.212V315a15,15,0,0,0,30,0V51.212l49.394,49.394a15,15,0,0,0,21.212-21.213l-75-75a15,15,0,0,0-21.213,0l-75,75a15,15,0,0,0,21.213,21.213Z"
-                                            transform="translate(330 -74.999) rotate(90)"
-                                        />
-                                    </svg>
-                                </span>
-                            </a>
+                                    viewBox="0 0 249.734 249.734"
+                                >
+                                    <path
+                                        d="M0,0C43.6,43.6,130.1,45.237,175.336,0c-45.251,45.251-51.313,126.528,0,177.841C123.259,125.763,47.245,130.6,0,177.841,46.912,130.929,43.567,43.567,0,0Z"
+                                        transform="translate(125.753) rotate(45)"
+                                    />
+                                </svg>
+                            </div>
+                            <div className={`${styles.link}`}>
+                                <a
+                                    data-mouse={'hide'}
+                                    // className={`${styles.link}`}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href={project.link}
+                                >
+                                    see more
+                                    <span data-mouse={'hide'} className={`${styles.arrow}`}>
+                                        <svg viewBox="0 0 330.001 180">
+                                            <path
+                                                d="M100.606,100.606,150,51.212V315a15,15,0,0,0,30,0V51.212l49.394,49.394a15,15,0,0,0,21.212-21.213l-75-75a15,15,0,0,0-21.213,0l-75,75a15,15,0,0,0,21.213,21.213Z"
+                                                transform="translate(330 -74.999) rotate(90)"
+                                            />
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </>
             );
         });
 
--- utils/projects.ts
+++ utils/projects.ts
@@ -5,7 +5,7 @@ const projects = [
         description: `
         This is a project I made for my bachelor thesis. 
         The goal was to make a self-driving rc car that runs on a Raspberry Pi.
-        It uses a single camera to observe the road and sends the images trough a neural network to determine the steering angle.
+        It uses a single camera to observe the road and sends the captured images trough a neural network to determine the steering angle.
         `,
         image: 'projects/selfdriving-car.png',
         link: 'https://drive.google.com/file/d/1Se7thaTL9O-rFyRB0GHqsoj-M2OMcF_e/view?usp=sharing',
@@ -23,6 +23,17 @@ const projects = [
     },
     {
         id: 3,
+        name: 'self driving car',
+        description: `
+        This is a project I made for my bachelor thesis. 
+        The goal was to make a self-driving rc car that runs on a Raspberry Pi.
+        It uses a single camera to observe the road and sends the images trough a neural network to determine the steering angle.
+        `,
+        image: 'projects/selfdriving-car.png',
+        link: 'https://drive.google.com/file/d/1Se7thaTL9O-rFyRB0GHqsoj-M2OMcF_e/view?usp=sharing',
+    },
+    {
+        id: 4,
         name: 'self driving car',
         description: `
         This is a project I made for my bachelor thesis. 
