aee556c929a92930d1280d4637138468a8f36a86
Felix Vandemaele <vandemaelefelix@gmail.com>
2022-04-10 22:33:45 +0200

    Touch support matterjs

--- components/TechnologiesMobile.tsx
+++ components/TechnologiesMobile.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 // import technologies from '../public/images/technologies/technologies';
 import tools from '../utils/tools';
 import Image from 'next/image';
+import { clear } from 'console';
 
 const STATIC_DENSITY = 80;
 
@@ -14,6 +15,8 @@ interface Icon {
 
 export default function TechnologiesMobile() {
     const [selectedIcon, setSelectedIcon] = useState<Icon | null>(null);
+    // const [showOverlay, setShowOverlay] = useState(true);
+    // const [overlayTimeout, setOverlayTimeout] = useState<any | null>(null);
 
     const sceneContainerRef = useRef<HTMLDivElement>(null);
     const sceneRef = useRef<HTMLCanvasElement>(null);
@@ -100,6 +103,18 @@ export default function TechnologiesMobile() {
                 }
             });
 
+            // @ts-ignore
+            mouseConstraint.mouse.element.removeEventListener('mousewheel', mouseConstraint.mouse.mousewheel);
+            // @ts-ignore
+            mouseConstraint.mouse.element.removeEventListener('DOMMouseScroll', mouseConstraint.mouse.mousewheel);
+
+            // // @ts-ignore
+            // mouseConstraint.mouse.element.removeEventListener('touchmove', mouseConstraint.mouse.mousemove);
+            // // @ts-ignore
+            // mouseConstraint.mouse.element.removeEventListener('touchstart', mouseConstraint.mouse.mousedown);
+            // // @ts-ignore
+            // mouseConstraint.mouse.element.removeEventListener('touchend', mouseConstraint.mouse.mouseup);
+
             World.add(engine.world, mouseConstraint);
 
             World.add(engine.world, [floor, leftWall, rightWall, roof]);
@@ -112,96 +127,55 @@ export default function TechnologiesMobile() {
             setScene(render);
 
             window.addEventListener('resize', handleResize);
+
+            let touchStart: any = null;
+            mouseConstraint.mouse.element.addEventListener('touchstart', (event) => {
+                if (!mouseConstraint.body) {
+                    touchStart = event;
+                }
+            });
+
+            mouseConstraint.mouse.element.addEventListener('touchend', (event) => {
+                event.preventDefault();
+                touchStart = null;
+            });
+
+            mouseConstraint.mouse.element.addEventListener('touchmove', (event) => {
+                // console.log(event);
+                // console.log(mouseConstraint.body);
+                if (!mouseConstraint.body && touchStart) {
+                    console.log('should be scrolling');
+                    event.preventDefault();
+                    let start = touchStart.touches[0].clientY;
+                    let end = event.touches[0].clientY;
+                    let delta = start - end;
+
+                    console.log(`
+                        Start: ${start}
+                        End: ${end}
+                        Delta: ${delta}
+                    `);
+
+                    window.scrollTo(0, window.scrollY + delta);
+                    touchStart = event;
+                }
+            });
         }
     };
 
     useEffect(() => {
-        // if (sceneContainerRef.current && sceneRef.current) {
-        //     let Engine = Matter.Engine;
-        //     let Render = Matter.Render;
-        //     let World = Matter.World;
-        //     let Bodies = Matter.Bodies;
-        //     let Mouse = Matter.Mouse;
-        //     let MouseConstraint = Matter.MouseConstraint;
-
-        //     let engine = Engine.create();
-
-        //     let render = Render.create({
-        //         element: sceneContainerRef.current,
-        //         engine: engine,
-        //         canvas: sceneRef.current,
-        //         options: {
-        //             background: 'rgba(0, 0, 0, 0)',
-        //             wireframes: false,
-        //         },
-        //     });
-
-        //     const floor = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
-        //         isStatic: true,
-        //     });
-
-        //     const leftWall = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
-        //         isStatic: true,
-        //     });
-        //     const rightWall = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
-        //         isStatic: true,
-        //     });
-        //     const roof = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
-        //         isStatic: true,
-        //     });
-
-        //     const icons: any = [];
-
-        //     technologies.forEach((tech: Icon) => {
-        //         const ball = Bodies.circle(150, 0, 50, {
-        //             label: JSON.stringify(tech),
-        //             restitution: 0.3,
-        //             render: {
-        //                 sprite: {
-        //                     texture: `/images/technologies/${tech.path}`,
-        //                     xScale: 0.5,
-        //                     yScale: 0.5,
-        //                 },
-        //                 fillStyle: 'yellow',
-        //             },
-        //         });
-        //         icons.push(ball);
-        //     });
-
-        //     var mouse = Mouse.create(render.canvas),
-        //         mouseConstraint = MouseConstraint.create(engine, {
-        //             mouse: mouse,
-        //             constraint: {
-        //                 stiffness: 0.3,
-        //                 render: {
-        //                     visible: false,
-        //                 },
-        //             } as Matter.Constraint,
-        //         });
-
-        //     Matter.Events.on(mouseConstraint, 'mousedown', function (event: any) {
-        //         if (mouseConstraint.body) {
-        //             const label: Icon = JSON.parse(mouseConstraint.body.label);
-        //             console.log(mouseConstraint.body);
-        //             setSelectedIcon(label);
-        //         }
-        //     });
-
-        //     World.add(engine.world, mouseConstraint);
-
-        //     World.add(engine.world, [floor, leftWall, rightWall, roof]);
-        //     World.add(engine.world, icons);
-
-        //     Engine.run(engine);
-        //     Render.run(render);
-
-        //     setContraints(sceneContainerRef.current.getBoundingClientRect());
-        //     setScene(render);
-
-        //     window.addEventListener('resize', handleResize);
-        // }
-
         setupScene();
+        // if (sceneRef.current) {
+        //     sceneRef.current.onwheel = function () {
+        //         window.addEventListener(
+        //             'DOMMouseScroll',
+        //             (e) => {
+        //                 console.log(e);
+        //             },
+        //             true
+        //         );
+        //     };
+        // }
 
         return () => {
             window.removeEventListener('resize', handleResize);
@@ -280,6 +254,22 @@ export default function TechnologiesMobile() {
 
     return (
         <section id={'spotlightSection'} className={`${styles.container}`}>
+            {/* <div
+                onTouchStart={() => {
+                    if (overlayTimeout) {
+                        clearTimeout(overlayTimeout);
+                    }
+                    setOverlayTimeout(
+                        setTimeout(() => {
+                            setShowOverlay(false);
+                        }, 50)
+                    );
+                }}
+                onTouchEnd={() => {
+                    setShowOverlay(true);
+                }}
+                className={`${styles.overlay} ${showOverlay ? '' : styles.hide}`}
+            ></div> */}
             <div
                 onClick={() => {
                     setSelectedIcon(null);
