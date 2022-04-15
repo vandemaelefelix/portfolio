74c8fcc64dbc2060a1c8f16774deff0a32a47151
Felix Vandemaele <vandemaelefelix@gmail.com>
2022-04-15 18:35:55 +0200

    Added alternating background-color for projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -91,103 +91,103 @@ export default function Projects({ isMobile }: Props) {
 
         projects.forEach((project) => {
             projectsContent.push(
-                <div data-mouse={'horizontal'} key={project.id}>
-                    <div
-                        data-mouse={'horizontal'}
-                        key={project.id}
-                        id={`section${project.id}`}
-                        className={`${styles.projectContainer}`}
-                    >
-                        <div data-mouse={'horizontal'} className={`${styles.project}`}>
-                            <h1 data-mouse={'horizontal'} className={`${styles.title}`}>
-                                {project.name}
-                            </h1>
-                            <p data-mouse={'horizontal'} className={`${styles.description}`}>
-                                {project.description}
-                            </p>
-                            <div data-mouse={'horizontal'} className={`${styles.imageContainer}`}>
-                                <svg
-                                    ref={(element) => {
-                                        if (parrallaxRefs.current) {
-                                            parrallaxRefs.current[parrallaxRefs.current.length + 1] = {
-                                                element: element,
-                                                options: { speed: 0.012, direction: 'invert' },
-                                            };
-                                        }
-                                    }}
-                                    data-mouse={'horizontal'}
-                                    className={`
+                <div
+                    data-test={'nothing'}
+                    key={project.id}
+                    id={`section${project.id}`}
+                    className={`${styles.projectContainer}`}
+                >
+                    <div data-test={'nothing'} className={`${styles.project}`}>
+                        <h1 data-test={'nothing'} className={`${styles.title}`}>
+                            {project.name}
+                        </h1>
+                        <p data-test={'nothing'} className={`${styles.description}`}>
+                            {project.description}
+                        </p>
+                        <div data-test={'nothing'} className={`${styles.imageContainer}`}>
+                            <svg
+                                ref={(element) => {
+                                    if (parrallaxRefs.current) {
+                                        parrallaxRefs.current[parrallaxRefs.current.length + 1] = {
+                                            element: element,
+                                            options: { speed: 0.012, direction: 'invert' },
+                                        };
+                                    }
+                                }}
+                                data-test={'nothing'}
+                                className={`
                                     ${styles.imageIcon}
                                     ${styles.top}
                                 `}
-                                    viewBox="0 0 134.46 134.46"
-                                >
-                                    <path
-                                        d="M0,0C23.477,23.477,70.047,24.356,94.4,0c-24.364,24.364-27.628,68.124,0,95.752-28.039-28.039-68.966-25.438-94.4,0C25.258,70.494,23.457,23.457,0,0Z"
-                                        transform="translate(67.707) rotate(45)"
-                                    />
-                                </svg>
-                                <div data-mouse={'horizontal'} className={`${styles.imageWrapper}`}>
-                                    <Image
-                                        className={styles.image}
-                                        alt={project.name}
-                                        src={`/images/${project.image}`}
-                                        layout={'fill'}
-                                        objectFit={'cover'}
-                                        loading={'lazy'}
-                                        data-mouse={'horizontal'}
-                                        // placeholder={'blur'}
-                                    ></Image>
-                                </div>
-                                <div data-mouse={'horizontal'} className={styles.imageOverlay}></div>
-                                <svg
-                                    ref={(element) => {
-                                        if (parrallaxRefs.current) {
-                                            parrallaxRefs.current[parrallaxRefs.current.length + 1] = {
-                                                element: element,
-                                                options: { speed: 0.012, direction: 'invert' },
-                                            };
-                                        }
-                                    }}
-                                    data-mouse={'horizontal'}
-                                    className={`
+                                viewBox="0 0 134.46 134.46"
+                            >
+                                <path
+                                    d="M0,0C23.477,23.477,70.047,24.356,94.4,0c-24.364,24.364-27.628,68.124,0,95.752-28.039-28.039-68.966-25.438-94.4,0C25.258,70.494,23.457,23.457,0,0Z"
+                                    transform="translate(67.707) rotate(45)"
+                                />
+                            </svg>
+                            <div data-test={'nothing'} className={`${styles.imageWrapper}`}>
+                                <Image
+                                    className={styles.image}
+                                    alt={project.name}
+                                    src={`/images/${project.image}`}
+                                    layout={'fill'}
+                                    objectFit={'cover'}
+                                    loading={'lazy'}
+                                    data-test={'nothing'}
+                                    // placeholder={'blur'}
+                                ></Image>
+                            </div>
+                            <div data-test={'nothing'} className={styles.imageOverlay}></div>
+                            <svg
+                                ref={(element) => {
+                                    if (parrallaxRefs.current) {
+                                        parrallaxRefs.current[parrallaxRefs.current.length + 1] = {
+                                            element: element,
+                                            options: { speed: 0.012, direction: 'invert' },
+                                        };
+                                    }
+                                }}
+                                data-test={'nothing'}
+                                className={`
                                     ${styles.imageIcon}
                                     ${styles.bottom}
                                 `}
-                                    viewBox="0 0 249.734 249.734"
-                                >
-                                    <path
-                                        d="M0,0C43.6,43.6,130.1,45.237,175.336,0c-45.251,45.251-51.313,126.528,0,177.841C123.259,125.763,47.245,130.6,0,177.841,46.912,130.929,43.567,43.567,0,0Z"
-                                        transform="translate(125.753) rotate(45)"
-                                    />
-                                </svg>
-                            </div>
-                            {project.link != '' ? (
-                                <div data-mouse={'horizontal'} className={`${styles.link}`}>
-                                    <a
-                                        data-mouse={'hide'}
-                                        // className={`${styles.link}`}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        href={project.link}
-                                    >
-                                        see more
-                                        <span data-mouse={'hide'} className={`${styles.arrow}`}>
-                                            <svg viewBox="0 0 330.001 180">
-                                                <path
-                                                    d="M100.606,100.606,150,51.212V315a15,15,0,0,0,30,0V51.212l49.394,49.394a15,15,0,0,0,21.212-21.213l-75-75a15,15,0,0,0-21.213,0l-75,75a15,15,0,0,0,21.213,21.213Z"
-                                                    transform="translate(330 -74.999) rotate(90)"
-                                                />
-                                            </svg>
-                                        </span>
-                                    </a>
-                                </div>
-                            ) : (
-                                <></>
-                            )}
+                                viewBox="0 0 249.734 249.734"
+                            >
+                                <path
+                                    d="M0,0C43.6,43.6,130.1,45.237,175.336,0c-45.251,45.251-51.313,126.528,0,177.841C123.259,125.763,47.245,130.6,0,177.841,46.912,130.929,43.567,43.567,0,0Z"
+                                    transform="translate(125.753) rotate(45)"
+                                />
+                            </svg>
                         </div>
+                        {project.link != '' ? (
+                            <div data-test={'nothing'} className={`${styles.link}`}>
+                                <a
+                                    data-mouse={'hide'}
+                                    // className={`${styles.link}`}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href={project.link}
+                                >
+                                    see more
+                                    <span data-mouse={'hide'} className={`${styles.arrow}`}>
+                                        <svg viewBox="0 0 330.001 180">
+                                            <path
+                                                d="M100.606,100.606,150,51.212V315a15,15,0,0,0,30,0V51.212l49.394,49.394a15,15,0,0,0,21.212-21.213l-75-75a15,15,0,0,0-21.213,0l-75,75a15,15,0,0,0,21.213,21.213Z"
+                                                transform="translate(330 -74.999) rotate(90)"
+                                            />
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
                     </div>
                 </div>
+                // <div  data-test={'nothing'} key={project.id}>
+                // </div>
             );
         });
 
