6b925fb2552bd13abb747eea7b6c59fe104ada90
Felix Vandemaele <vandemaelefelix@gmail.com>
2022-04-23 17:27:01 +0200

    Fixed scroll issue on mobile tools section

--- components/TechnologiesMobile.tsx
+++ components/TechnologiesMobile.tsx
@@ -23,6 +23,11 @@ export default function TechnologiesMobile() {
 
     const [constraints, setContraints] = useState<any>();
     const [scene, setScene] = useState<any>();
+    const [sceneRenderer, setSceneRenderer] = useState<{ engine: any; render: any; runner: any; element: any } | null>(
+        null
+    );
+
+    const [resetScene, setResetScene] = useState(false);
 
     const handleResize = () => {
         if (sceneContainerRef.current) {
@@ -30,16 +35,240 @@ export default function TechnologiesMobile() {
         }
     };
 
-    const setupScene = () => {
+    // const setupScene = () => {
+    //     if (sceneContainerRef.current && sceneRef.current) {
+    //         let Engine = Matter.Engine;
+    //         let Render = Matter.Render;
+    //         let Runner = Matter.Runner;
+    //         let Bodies = Matter.Bodies;
+    //         let Mouse = Matter.Mouse;
+    //         let MouseConstraint = Matter.MouseConstraint;
+    //         let Composite = Matter.Composite;
+
+    //         if (sceneRenderer) {
+    //             console.log('Clearing scene');
+    //             Composite.clear(sceneRenderer.engine.world, false, true);
+    //             Engine.clear(sceneRenderer.engine);
+    //             Render.stop(sceneRenderer.render);
+    //             Runner.stop(sceneRenderer.runner);
+    //             sceneRenderer.render.canvas.remove();
+    //             sceneRenderer.render.canvas = null;
+    //             sceneRenderer.render.context = null;
+    //             sceneRenderer.render.textures = {};
+
+    //             sceneRenderer.element.replaceWith(sceneRenderer.element.cloneNode(true));
+
+    //             // mouseConstraint.mouse.element.addEventListener('touchstart', handleTouchStart, {
+    //             //     passive: true,
+    //             // });
+    //             // mouseConstraint.mouse.element.addEventListener('touchend', handleTouchEnd, {
+    //             //     passive: true,
+    //             // });
+    //             // mouseConstraint.mouse.element.addEventListener('touchmove', handleTouchMove, {
+    //             //     passive: true,
+    //             // });
+    //         }
+
+    //         let engine = Engine.create();
+    //         let runner = Runner.create();
+
+    //         let render = Render.create({
+    //             element: sceneContainerRef.current,
+    //             engine: engine,
+    //             canvas: sceneRef.current,
+    //             options: {
+    //                 background: 'rgba(0, 0, 0, 0)',
+    //                 wireframes: false,
+    //             },
+    //         });
+
+    //         const floor = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
+    //             isStatic: true,
+    //         });
+
+    //         const leftWall = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
+    //             isStatic: true,
+    //         });
+    //         const rightWall = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
+    //             isStatic: true,
+    //         });
+    //         const roof = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
+    //             isStatic: true,
+    //         });
+
+    //         const icons: any = [];
+
+    //         tools.forEach((tech: Icon) => {
+    //             const ball = Bodies.circle(Math.random() * 150, 0, 40, {
+    //                 label: JSON.stringify(tech),
+    //                 restitution: 0.1,
+    //                 render: {
+    //                     sprite: {
+    //                         texture: `/images/tools/${tech.path}`,
+    //                         // texture: `/images/technologies/${tech.path}`,
+    //                         xScale: 0.4,
+    //                         yScale: 0.4,
+    //                     },
+    //                     fillStyle: 'yellow',
+    //                 },
+    //             });
+    //             icons.push(ball);
+    //         });
+
+    //         var mouse = Mouse.create(render.canvas),
+    //             mouseConstraint = MouseConstraint.create(engine, {
+    //                 mouse: mouse,
+    //                 constraint: {
+    //                     stiffness: 0.3,
+    //                     render: {
+    //                         visible: false,
+    //                     },
+    //                 } as Matter.Constraint,
+    //             });
+
+    //         Matter.Events.on(mouseConstraint, 'mousedown', function (event: any) {
+    //             if (mouseConstraint.body) {
+    //                 // console.log(mouseConstraint.body.label);
+    //                 let label: Icon | null;
+    //                 try {
+    //                     label = JSON.parse(mouseConstraint.body.label);
+    //                 } catch (error) {
+    //                     label = null;
+    //                 }
+    //                 console.log(mouseConstraint.body);
+    //                 setSelectedIcon(label);
+    //             }
+    //         });
+
+    //         // console.log(mouseConstraint.mouse.element);
+
+    //         // @ts-ignore
+    //         mouseConstraint.mouse.element.removeEventListener('mousewheel', mouseConstraint.mouse.mousewheel, {
+    //             passive: true,
+    //         });
+    //         // @ts-ignore
+    //         mouseConstraint.mouse.element.removeEventListener('DOMMouseScroll', mouseConstraint.mouse.mousewheel, {
+    //             passive: true,
+    //         });
+
+    //         Composite.add(engine.world, mouseConstraint);
+
+    //         Composite.add(engine.world, [floor, leftWall, rightWall, roof]);
+    //         Composite.add(engine.world, icons);
+
+    //         // Engine.run(engine);
+    //         Matter.Runner.run(engine);
+    //         Render.run(render);
+
+    //         setContraints(sceneContainerRef.current.getBoundingClientRect());
+    //         setScene(render);
+
+    //         window.addEventListener('resize', handleResize);
+
+    //         const handleTouchStart = (event: any) => {
+    //             if (!mouseConstraint.body) {
+    //                 touchStart = event;
+    //             }
+    //         };
+    //         const handleTouchEnd = (event: any) => {
+    //             touchStart = null;
+    //         };
+    //         const handleTouchMove = (event: any) => {
+    //             // console.log(event);
+    //             // console.log(mouseConstraint.body);
+    //             if (!mouseConstraint.body && touchStart) {
+    //                 // event.preventDefault();
+    //                 let start = touchStart.touches[0].clientY;
+    //                 let end = event.touches[0].clientY;
+    //                 let delta = start - end;
+
+    //                 window.scrollTo(0, window.scrollY + delta);
+    //                 touchStart = event;
+    //             }
+    //         };
+
+    //         let touchStart: any = null;
+    //         mouseConstraint.mouse.element.addEventListener('touchstart', handleTouchStart, { passive: true });
+
+    //         mouseConstraint.mouse.element.addEventListener('touchend', handleTouchEnd, { passive: true });
+
+    //         mouseConstraint.mouse.element.addEventListener('touchmove', handleTouchMove, { passive: true });
+
+    //         // let touchStart: any = null;
+    //         // mouseConstraint.mouse.element.addEventListener(
+    //         //     'touchstart',
+    //         //     (event) => {
+    //         //         if (!mouseConstraint.body) {
+    //         //             touchStart = event;
+    //         //         }
+    //         //     },
+    //         //     { passive: true }
+    //         // );
+
+    //         // mouseConstraint.mouse.element.addEventListener(
+    //         //     'touchend',
+    //         //     (event) => {
+    //         //         // event.preventDefault();
+    //         //         touchStart = null;
+    //         //     },
+    //         //     { passive: true }
+    //         // );
+
+    //         // mouseConstraint.mouse.element.addEventListener(
+    //         //     'touchmove',
+    //         //     (event) => {
+    //         //         // console.log(event);
+    //         //         // console.log(mouseConstraint.body);
+    //         //         if (!mouseConstraint.body && touchStart) {
+    //         //             // event.preventDefault();
+    //         //             let start = touchStart.touches[0].clientY;
+    //         //             let end = event.touches[0].clientY;
+    //         //             let delta = start - end;
+
+    //         //             window.scrollTo(0, window.scrollY + delta);
+    //         //             touchStart = event;
+    //         //         }
+    //         //     },
+    //         //     { passive: true }
+    //         // );
+
+    //         setSceneRenderer({
+    //             engine: engine,
+    //             render: render,
+    //             runner: runner,
+    //             element: mouseConstraint.mouse.element,
+    //         });
+    //     }
+    // };
+    useEffect(() => {
+        let handleTouchStart: any;
+        let handleTouchEnd: any;
+        let handleTouchMove: any;
         if (sceneContainerRef.current && sceneRef.current) {
             let Engine = Matter.Engine;
             let Render = Matter.Render;
-            let World = Matter.World;
+            let Runner = Matter.Runner;
             let Bodies = Matter.Bodies;
             let Mouse = Matter.Mouse;
             let MouseConstraint = Matter.MouseConstraint;
+            let Composite = Matter.Composite;
+
+            if (sceneRenderer) {
+                console.log('Clearing scene');
+                Composite.clear(sceneRenderer.engine.world, false, true);
+                Engine.clear(sceneRenderer.engine);
+                Render.stop(sceneRenderer.render);
+                Runner.stop(sceneRenderer.runner);
+                sceneRenderer.render.canvas.remove();
+                sceneRenderer.render.canvas = null;
+                sceneRenderer.render.context = null;
+                sceneRenderer.render.textures = {};
+
+                sceneRenderer.element.replaceWith(sceneRenderer.element.cloneNode(true));
+            }
 
             let engine = Engine.create();
+            let runner = Runner.create();
 
             let render = Render.create({
                 element: sceneContainerRef.current,
@@ -53,16 +282,28 @@ export default function TechnologiesMobile() {
 
             const floor = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
                 isStatic: true,
+                render: {
+                    fillStyle: 'rgba(0, 0, 0, 0)',
+                },
             });
 
             const leftWall = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
                 isStatic: true,
+                render: {
+                    fillStyle: 'rgba(0, 0, 0, 0)',
+                },
             });
             const rightWall = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
                 isStatic: true,
+                render: {
+                    fillStyle: 'rgba(0, 0, 0, 0)',
+                },
             });
             const roof = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
                 isStatic: true,
+                render: {
+                    fillStyle: 'rgba(0, 0, 0, 0)',
+                },
             });
 
             const icons: any = [];
@@ -97,30 +338,32 @@ export default function TechnologiesMobile() {
 
             Matter.Events.on(mouseConstraint, 'mousedown', function (event: any) {
                 if (mouseConstraint.body) {
-                    const label: Icon = JSON.parse(mouseConstraint.body.label);
+                    let label: Icon | null;
+                    try {
+                        label = JSON.parse(mouseConstraint.body.label);
+                    } catch (error) {
+                        label = null;
+                    }
                     console.log(mouseConstraint.body);
                     setSelectedIcon(label);
                 }
             });
 
             // @ts-ignore
-            mouseConstraint.mouse.element.removeEventListener('mousewheel', mouseConstraint.mouse.mousewheel);
+            mouseConstraint.mouse.element.removeEventListener('mousewheel', mouseConstraint.mouse.mousewheel, {
+                passive: true,
+            });
             // @ts-ignore
-            mouseConstraint.mouse.element.removeEventListener('DOMMouseScroll', mouseConstraint.mouse.mousewheel);
+            mouseConstraint.mouse.element.removeEventListener('DOMMouseScroll', mouseConstraint.mouse.mousewheel, {
+                passive: true,
+            });
 
-            // // @ts-ignore
-            // mouseConstraint.mouse.element.removeEventListener('touchmove', mouseConstraint.mouse.mousemove);
-            // // @ts-ignore
-            // mouseConstraint.mouse.element.removeEventListener('touchstart', mouseConstraint.mouse.mousedown);
-            // // @ts-ignore
-            // mouseConstraint.mouse.element.removeEventListener('touchend', mouseConstraint.mouse.mouseup);
+            Composite.add(engine.world, mouseConstraint);
 
-            World.add(engine.world, mouseConstraint);
+            Composite.add(engine.world, [floor, leftWall, rightWall, roof]);
+            Composite.add(engine.world, icons);
 
-            World.add(engine.world, [floor, leftWall, rightWall, roof]);
-            World.add(engine.world, icons);
-
-            Engine.run(engine);
+            Matter.Runner.run(engine);
             Render.run(render);
 
             setContraints(sceneContainerRef.current.getBoundingClientRect());
@@ -128,59 +371,46 @@ export default function TechnologiesMobile() {
 
             window.addEventListener('resize', handleResize);
 
-            let touchStart: any = null;
-            mouseConstraint.mouse.element.addEventListener('touchstart', (event) => {
+            handleTouchStart = (event: any) => {
                 if (!mouseConstraint.body) {
                     touchStart = event;
                 }
-            });
-
-            mouseConstraint.mouse.element.addEventListener('touchend', (event) => {
-                event.preventDefault();
+            };
+            handleTouchEnd = (event: any) => {
                 touchStart = null;
-            });
-
-            mouseConstraint.mouse.element.addEventListener('touchmove', (event) => {
-                // console.log(event);
-                // console.log(mouseConstraint.body);
+            };
+            handleTouchMove = (event: any) => {
                 if (!mouseConstraint.body && touchStart) {
-                    console.log('should be scrolling');
-                    event.preventDefault();
                     let start = touchStart.touches[0].clientY;
                     let end = event.touches[0].clientY;
                     let delta = start - end;
 
-                    console.log(`
-                        Start: ${start}
-                        End: ${end}
-                        Delta: ${delta}
-                    `);
-
                     window.scrollTo(0, window.scrollY + delta);
                     touchStart = event;
                 }
+            };
+
+            let touchStart: any = null;
+            mouseConstraint.mouse.element.addEventListener('touchstart', handleTouchStart, { passive: true });
+
+            mouseConstraint.mouse.element.addEventListener('touchend', handleTouchEnd, { passive: true });
+
+            mouseConstraint.mouse.element.addEventListener('touchmove', handleTouchMove, { passive: true });
+
+            setSceneRenderer({
+                engine: engine,
+                render: render,
+                runner: runner,
+                element: mouseConstraint.mouse.element,
             });
         }
-    };
-
-    useEffect(() => {
-        setupScene();
-        // if (sceneRef.current) {
-        //     sceneRef.current.onwheel = function () {
-        //         window.addEventListener(
-        //             'DOMMouseScroll',
-        //             (e) => {
-        //                 console.log(e);
-        //             },
-        //             true
-        //         );
-        //     };
-        // }
-
         return () => {
+            mouseConstraint.mouse.element.removeEventListener('touchstart', handleTouchStart);
+            mouseConstraint.mouse.element.removeEventListener('touchend', handleTouchEnd);
+            mouseConstraint.mouse.element.removeEventListener('touchmove', handleTouchMove);
             window.removeEventListener('resize', handleResize);
         };
-    }, []);
+    }, [resetScene]);
 
     useEffect(() => {
         if (constraints) {
@@ -254,34 +484,17 @@ export default function TechnologiesMobile() {
 
     return (
         <section id={'spotlightSection'} className={`${styles.container}`}>
-            {/* <div
-                onTouchStart={() => {
-                    if (overlayTimeout) {
-                        clearTimeout(overlayTimeout);
-                    }
-                    setOverlayTimeout(
-                        setTimeout(() => {
-                            setShowOverlay(false);
-                        }, 50)
-                    );
-                }}
-                onTouchEnd={() => {
-                    setShowOverlay(true);
-                }}
-                className={`${styles.overlay} ${showOverlay ? '' : styles.hide}`}
-            ></div> */}
+            <h1 className={`${styles.title}`}>Tools I use</h1>
             <div
                 onClick={() => {
                     setSelectedIcon(null);
-                    setupScene();
+                    setResetScene(!resetScene);
                 }}
                 className={`${styles.menuIcon}`}
                 data-mouse="hide"
             >
-                <p data-mouse="hide" className={styles.menuIconText}>
-                    reset
-                </p>
-                <span data-mouse="hide" className={styles.menuIconDot}></span>
+                <p className={styles.menuIconText}>reset</p>
+                <span className={styles.menuIconDot}></span>
             </div>
 
             <div className={`${styles.sceneContainer}`} ref={sceneContainerRef}>
